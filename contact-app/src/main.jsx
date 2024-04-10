09d7ee1ea7fcaf8aabe902968fd77242c79eb9e1
Akua Serwaa Nkrumah <nkrumahakua2002@gmail.com>
2024-04-10 22:04:22 +0000

    Contact Management App

--- contact-app/src/main.jsx
+++ contact-app/src/main.jsx
@@ -1,16 +1,19 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import Root, {
-  loader as rootLoader,
-  action as rootAction,
-} from "./routes/root";
-import ErrorPage from "./routes/error-page";
-import Contact, { loader as contactLoader, action as contactAction } from "./routes/contact";
-import EditContact, { action as editAction } from "./routes/edit";
-import { action as deleteAction } from "./routes/destroy";
-import Index from "./routes/index";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Contact,{
+  loader as contactLoader,
+  action as contactAction,
+} from "./routes/contact";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import "./index.css";
+import ErrorPage from "./error-page";
+import Root, { loader as rootLoader, action as rootAction,} from "./routes/root";
+import EditContact,{action as editAction,} from "./routes/edit";
+import { action as destroyAction } from "./routes/destroy";
+import Index from "./routes/index";
 
 const router = createBrowserRouter([
   {
@@ -28,27 +31,21 @@ const router = createBrowserRouter([
             path: "contacts/:contactId",
             element: <Contact />,
             loader: contactLoader,
-            action: contactAction
-          },
-          {
-            path: "contacts/:contactId/edit",
-            element: <EditContact />,
-            loader: contactLoader,
-            action: editAction,
-          },
-        ],
-      }
-    ]
-  },
-  {
-    path: "contacts/:contactId",
-    element: <Contact />,
-  },
-  {
-    path: "contacts/:contactId/destroy",
-    action: deleteAction,
-    errorElement: <div>Oops! There was an error</div>,
-  },
+            action: contactAction,
+          }],
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
+      },
+      {
+        path: "contacts/:contactId/destroy",
+        action: destroyAction,
+      },
+    ],
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
